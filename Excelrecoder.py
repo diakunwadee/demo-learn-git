828000a3e7c05e02188eb39a3b39d1021aa95cba
Kunwa <dearkunwadee@gmail.com>
2018-11-15 19:50:43 +0700

    first commit

---
+++ Excelrecoder.py
@@ -0,0 +1,22 @@
+from openpyxl import Workbook
+
+excelfile = Workbook()
+row = excelfile.active
+row.append(['Product','Amount','Price'])
+
+product = ''
+
+while product!= 's':
+    product = input("Enter Product Name: ")
+
+    if product!= 's':
+        amount = int(input("Enter Amount: "))
+        price = int(input("Price: "))
+        row.append([product, amount, price])
+    else: 
+        break
+
+filename = input("Enter file name: ")
+filename = filename + ".xlsx"
+excelfile.save(filename)
+print("Excel Saved: ", filename)
